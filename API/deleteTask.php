8b30966210a00abfb14c9f366018fa193f54f876
Tay9875 <96668181+Tay9875@users.noreply.github.com>
2024-11-01 03:10:06 +0100

    Update deleteTask.php

--- API/deleteTask.php
+++ API/deleteTask.php
@@ -1,8 +1,8 @@
 <?php
     $serveur="localhost";
     $dbname="db_collab";
-    $user="root";
-    $password="0000";
+    $user="XXXX";
+    $password="XXXX";
 
     try{
         $connexion =new PDO("mysql:host=$serveur;dbname=$dbname",$user,$password);
