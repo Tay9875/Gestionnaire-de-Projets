c756457f2454ea9b4c4805307c344e2239b4b6a1
Tay9875 <96668181+Tay9875@users.noreply.github.com>
2024-11-01 03:11:22 +0100

    Update mur_1.php

--- API/mur_1.php
+++ API/mur_1.php
@@ -1,8 +1,8 @@
 <?php
     $serveur="localhost";
     $dbname="db_collab";
-    $user="root";
-    $password="0000";
+    $user="XXXX";
+    $password="XXXX";
 
     if($_SESSION['id_p']){
         try{
